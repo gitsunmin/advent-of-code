a1b2a1b7169ba8727cbee8a1d68a5618bb17b932
김선민 <tjsals116@gmail.com>
2023-11-28 13:29:20 +0900

    🚩 cleared 2022 day 1 - part1 by rust

--- 2022/rust/src/day1/index.rs
+++ 2022/rust/src/day1/index.rs
@@ -1,10 +1,33 @@
 use std::fs;
 
-pub fn run() {
-    println!("day1 Hello, world!");
-
-    let contents = fs::read_to_string("src/../../inputs/day1.txt")
+fn read_input() -> String {
+    return fs::read_to_string("src/../../inputs/day1.txt")
         .expect("Something went wrong reading the file");
+}
 
-    println!("contents: {:?}", contents);
+fn parse_input(input: String) -> Vec<Vec<i32>> {
+    return input
+        .split("\n\n")
+        .map(|x| x.split("\n").collect::<Vec<&str>>())
+        .map(|x| x.iter().map(|y| y.parse().unwrap()).collect::<Vec<i32>>())
+        .collect::<Vec<Vec<i32>>>();
+}
+
+fn solve_part_1(input: Vec<Vec<i32>>) -> i32 {
+    return input
+        .iter()
+        .map(|x| x.iter().sum())
+        .collect::<Vec<i32>>()
+        .iter()
+        .copied()
+        .max()
+        .unwrap();
+}
+
+pub fn run() -> (i32, i32) {
+    let input = read_input();
+
+    let parsed_input = parse_input(input);
+
+    return (solve_part_1(parsed_input), 0);
 }
--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,9 +1,13 @@
-
-#[path = "day1/index.rs"] mod day1;
-
+#[path = "day1/index.rs"]
+mod day1;
 
 fn main() {
+    println!("##########################################\n");
     println!("Starting 2022 Advent of Code Rust solutions");
+    let (day1_part1, day1_part2) = day1::run();
 
-    day1::run();
+    println!("------------------------------------------");
+    println!("Day 1 | Part1 : {} | Part2 : {}", day1_part1, day1_part2);
+
+    println!("\n##########################################");
 }
