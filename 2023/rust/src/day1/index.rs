3ef64cd41fc970d220512bd3501bebeb91586944
gitsunmin <tjsals116@gmail.com>
2023-12-03 23:15:47 +0900

    🚩 cleared 2023 day 1 - part 1 by rust

--- 2023/rust/src/day1/index.rs
+++ 2023/rust/src/day1/index.rs
@@ -1,3 +1,69 @@
-pub fn parse_input() {
-    println!("day1 Hello, world!");
+use regex::Regex;
+use std::fs;
+
+fn read_input() -> String {
+    return fs::read_to_string("src/../../inputs/day1.txt")
+        .expect("Something went wrong reading the file");
+}
+
+fn parse_input(input: String) -> Vec<String> {
+    return input
+        .split("\n")
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+}
+
+fn solve_part_1(input: Vec<String>) -> i32 {
+    let regex_only_number = Regex::new(r"\d").unwrap();
+    return input
+        .iter()
+        .map(|x| {
+            regex_only_number
+                .find_iter(x)
+                .map(|y| y.as_str())
+                .collect::<Vec<_>>()
+        })
+        .map(|x| vec![x[0], x[x.len() - 1]].join("").parse::<i32>().unwrap())
+        .sum::<i32>();
+}
+fn solve_part_2(input: Vec<String>) -> i32 {
+    let regex_only_number = Regex::new(r"one|two|three|four|five|six|seven|eight|nine").unwrap();
+
+    fn get_number(input: &str) -> &str {
+        match input {
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => "0",
+        }
+    }
+
+    return input
+        .iter()
+        .map(|x| {
+            regex_only_number
+                .find_iter(x)
+                .map(|y| get_number(y.as_str()))
+                .collect::<Vec<_>>()
+        })
+        .filter(|x| x.len() > 0)
+        .map(|x| vec![x[0], x[x.len() - 1]].join("").parse::<i32>().unwrap())
+        .sum::<i32>();
+}
+
+pub fn run() -> (i32, i32) {
+    let input = read_input();
+
+    let parsed_input = parse_input(input);
+
+    return (
+        solve_part_1(parsed_input.clone()),
+        solve_part_2(parsed_input.clone()),
+    );
 }
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,8 +1,13 @@
-#[path = "day1/index.rs"] mod day1;
-
+#[path = "day1/index.rs"]
+mod day1;
 
 fn main() {
+    println!("##########################################\n");
     println!("Starting 2023 Advent of Code Rust solutions");
+    let (day1_part1, day1_part2) = day1::run();
 
-    day1::parse_input();
+    println!("------------------------------------------");
+    println!("Day 1 | Part1 : {} | Part2 : {}", day1_part1, day1_part2);
+
+    println!("\n##########################################");
 }
